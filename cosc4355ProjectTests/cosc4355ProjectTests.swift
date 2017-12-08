65ae4601acb7aef3e7e4ed993316262d9994a909
Ron Borneo <ronborneo1@gmail.com>
2017-12-07 18:47:12 -0600

    added first test case

--- cosc4355ProjectTests/cosc4355ProjectTests.swift
+++ cosc4355ProjectTests/cosc4355ProjectTests.swift
@@ -11,27 +11,30 @@ import Firebase
 @testable import cosc4355Project
 
 class cosc4355ProjectTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testGetFormattedCurrency() {
+    XCTAssertEqual(Double.getFormattedCurrency(num: 10.03457), "$10.03")
+    XCTAssertEqual(Double.getFormattedCurrency(num: 0012.03457), "$12.03")
+    XCTAssertEqual(Double.getFormattedCurrency(num: 1000.301), "$1,000.30")
+    XCTAssertEqual(Double.getFormattedCurrency(num: 9999.999), "$10,000.00")
+    XCTAssertEqual(Double.getFormattedCurrency(num: 0.30), "$0.30")
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
