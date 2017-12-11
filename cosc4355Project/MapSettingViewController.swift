c5c5009360c8c9860c1c970fb5c046da38297d54
Qaem Parasla <qaemparasla@yahoo.com>
2017-12-11 16:45:45 -0600

    Revert "update"
    This reverts commit 0d4b8a43a472ae25f0032dd02078d185c3b110b8.

--- cosc4355Project/MapSettingViewController.swift
+++ cosc4355Project/MapSettingViewController.swift
@@ -2,56 +2,45 @@
 //  MapSettingViewController.swift
 //  cosc4355Project
 //
-//  Created by Qaem Parasla
+//  Created by Qaem on 12/4/17.
 //  Copyright © 2017 cosc4355. All rights reserved.
 //
 
 import UIKit
 
-protocol MapSettingDelegate: class {
-    func updateMaxTravelDistanceIn(miles: Int)
-}
-
 class MapSettingViewController: UIViewController {
+    
+    var parentView: MapsViewController?
+    weak var delegate: MapSettingProtocol?
     
     
     @IBOutlet weak var radiusInMileLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     
-    weak var delegate: MapSettingDelegate?
-    var radiusInMile: Int = 0
     
     @IBAction func sliderValueChanged(_ sender: UISlider)
     {
-        radiusInMileLabel.text = String(round(Double(slider.value))) + " mi"
+        radiusInMileLabel.text = String(round(Double(slider.value)))
     }
     
     
     @IBAction func saveMapSetting(_ sender: UIButton)
     {
-        if(delegate != nil)
-        {
-            delegate?.updateMaxTravelDistanceIn(miles: Int(round(slider.value)))
-        }
+        delegate?.updateMaxTravelDistanceIn(miles: Double(round(Double(slider.value))))
     }
+    
     
     func setSliderValue(value : Float)
     {
         slider.setValue(value, animated: false)
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if(Float(radiusInMile) < slider.minimumValue || Float(radiusInMile) > slider.maximumValue)
-        {
-            let midValue = (slider.maximumValue - slider.maximumValue)/2
-            slider.setValue(midValue, animated: true)
-        }
-        else
-        {
-            slider.setValue(Float(radiusInMile), animated: true)
-            radiusInMileLabel.text = String(round(slider.value))
-        }
+
+        // Do any additional setup after loading the view.
     }
+    
+    
 }
