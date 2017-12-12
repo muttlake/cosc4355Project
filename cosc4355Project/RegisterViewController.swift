2277403bd97886cb28aeea29bb96693bd40fb85c
Ron Borneo <ronborneo1@gmail.com>
2017-12-11 23:44:53 -0600

    slide up keyboard.. kind of

--- cosc4355Project/RegisterViewController.swift
+++ cosc4355Project/RegisterViewController.swift
@@ -63,6 +63,48 @@ class RegisterViewController: UIViewController, UIImagePickerControllerDelegate,
     }
   }
   
+  /** Slide keyboard - start */
+  var activeTextField: UITextField!
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    activeTextField = textField
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    NotificationCenter.default.removeObserver(self, name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.removeObserver(self, name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+  }
+
+  @objc func keyboardDidShow(notification:Notification){
+    let info:NSDictionary = notification.userInfo! as NSDictionary
+    let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    let keyboardY = self.view.frame.size.height - keyboardSize.height
+    
+    let editingTextFieldY:CGFloat! = self.activeTextField?.frame.origin.y
+    
+    if self.view.frame.origin.y >= 0 {
+      //Checking if the textfield is really hidden behind the keyboard
+      if editingTextFieldY > keyboardY - 60 {
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+          self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextFieldY! - (keyboardY - 60)), width: self.view.bounds.width,height: self.view.bounds.height)
+        }, completion: nil)
+      }
+    }
+    
+  }
+  
+  
+  @objc func keyboardWillHide(notification:Notification){
+    UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+      self.view.frame = CGRect(x: 0, y: 0,width: self.view.bounds.width, height: self.view.bounds.height)
+    }, completion: nil)
+    
+  }
+
+ /* Slide keyboard - end */
+  
+  
+  
   @IBAction func goBackButton(_ sender: UIButton) {
     dismiss(animated: true, completion: nil)
   }
@@ -114,10 +156,17 @@ class RegisterViewController: UIViewController, UIImagePickerControllerDelegate,
     passwordText.delegate = self
     confirmPasswordText.delegate = self
     nameText.delegate = self
+    
+    /* Slide keyboard */
+    let center: NotificationCenter = NotificationCenter.default;
+    center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+    center.addObserver(self, selector:#selector(keyboardWillHide(notification:)), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+
   }
   
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     view.endEditing(true)
+    textField.resignFirstResponder()
     return true
   }
   
